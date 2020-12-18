db87d8a8b40f3f17c8af2f770a84ae05ff1dfefb
bateng <fjl0120@163.com>
2020-12-18 15:18:45 +0800

    普通更新

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <input v-model="inputValue"/>
+    <button @click="handleClick">插入数据</button>
+    <hello-world v-for="(item,index) of list"
+                 :item="item"
+                 :index="index"
+                 @del="del"></hello-world>
+  </div>
+</template>
+
+<script>
+  import HelloWorld from './components/HelloWorld'
+
+  export default {
+    name: 'App',
+    components: {HelloWorld},
+    data() {
+      return {
+        inputValue: 'test',
+        list: []
+      }
+    },
+    methods: {
+      handleClick() {
+        this.list.push(this.inputValue);
+        this.inputValue = ''
+
+      },
+      del(index){
+        this.list = this.list.slice(0,index).concat(this.list.slice(index+1))
+      }
+    },
+    comments: {
+      'hello-world': HelloWorld
+    }
+
+  }
+</script>
+
+<style>
+
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<template>
+ <p @click="handleClick">{{item}}</p>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props:['item','index'],
+  methods:{
+    handleClick(){
+      this.$emit("del",this.index);
+    }
+  }
+
+}
+</script>
+
